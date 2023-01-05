f0cde6218c0ba5c885664151984c7ca1074dcc6e
gmk0 <mkgeorges63@gmail.com>
2023-01-05 21:07:18 +0100

    change Url

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{asset('build/assets/app-2a37e680.css')}}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <script src="{{url('build/assets/app-40420331.js')}}"></script>
+    <!-- Scripts -->
+    {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+</head>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="bg-white">
-        <div class="font-sans text-gray-900 ">
-            {{ $slot }}
-        </div>
-    </body>
+<body class="bg-white">
+    <div class="font-sans text-gray-900 ">
+        {{ $slot }}
+    </div>
+</body>
+
 </html>
