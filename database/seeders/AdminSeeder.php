a0904857a678f544b1f47485c6c444f84a13f6a1
gmk0 <mkgeorges63@gmail.com>
2023-01-09 22:06:01 +0100

    seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class AdminSeeder extends Seeder
 {
@@ -15,7 +16,19 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Admin::factory(1)->create();
+        \App\Models\Admin::create([
+        
+            //
+            'name' => "admin",
+            'email' => "admin@admin.com",
+            'mobile' => "0844720350",
+            'email_verified_at' => now(),
+
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => 'user.png'
+     
+        ]);
                
     }
 }
